cd1fcbed4794ee0ec21978f89dac9045ad1107e1
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-20 21:35:28 +0100

    create html template for components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import faker from "faker";
 
 export function App() {
-    return
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">{faker}</a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice post!</div>
+                </div>
+            </div>
+        </div>
+    )
 }
