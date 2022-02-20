b4e5c1f945dc3e0cff7372c57e942a4341d8e778
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-20 22:53:20 +0100

    add Content component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,21 +1,13 @@
 import React from "react"
-import faker from "faker"
 import { Avatar } from "./Avatar"
+import { Content } from "./Content";
 
 export function Comment(props) {
     console.log(props);
     return (
         <div className="comment">
-            <div className="content">
-                <Avatar />
-                <a href="/" className="author">
-                    {props.name}
-                </a>
-                <div className="metadata">
-                    <span className="date">{props.date}</span>
-                </div>
-                <div className="text">Nice post!</div>
-            </div>
+            <Avatar imgSrc={props.imgSrc} />
+            <Content name={props.name} date={props.date} content={props.content} />
         </div>
     )
 }
